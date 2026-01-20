74e4a536350132c3bf8d1b56908d6106e9696f63
alejandro-gonzales <agonzalesllontop05@gmail.com>
2026-01-20 14:56:16 -0500

    Add files via upload

---
+++ main.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>hola mundo</title>
+    <style>
+        p {background-color:  yellow;}
+        h1 {color: red;}
+    </style>
+</head>
+<body>
+    <h1>mano</h1>
+    <p>mano mano</p>
+    <?php
+        echo "<p>el dia de hoy es " . date("Y-m-d") . ".</p>"; 
+    ?>
+
+<scrpit>
+    
+</script>
+
+
+</body>
+</html>
+
